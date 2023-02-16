6ce70655ab7cb22ef2d4604cf9477d9d5c5f9679
Tim <ttruong576581@gmail.com>
2023-02-16 15:27:05 -0500

    Added Stuff

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    updateService();
+});
+
 function changeCounter(operation, player) {
     // Get the counter element
     let counter = 0;
@@ -12,12 +16,16 @@ function changeCounter(operation, player) {
     // Increment or decrement the counter value depending on the operation
     if (operation === "increment") {
         counterValue++;
-    } else if (operation === "decrement") {
+    } else if (operation === "decrement" && counterValue > 0) {
         counterValue--;
+    } else if (operation === "decrement" && counterValue <= 0) {
+        counterValue = 0;
     }
 
     // Update the counter element with the new value
     counter.innerText = counterValue.toString();
+
+    updateService(operation);
 }
 
 function newGame() {
@@ -35,36 +43,57 @@ function newGame() {
     player2_name.value = "";
     player1_action.classList.remove("active");
     player2_action.classList.remove("active");
+
+    let total_score = parseInt(player1_score.innerText) + parseInt(player2_score.innerText);
+
+    if (total_score == 0) {
+        let rand = Math.floor(Math.random() * 2);
+        if (rand == 0) {
+            player1_action.classList.add("active");
+        } else {
+            player2_action.classList.add("active");
+        }
+    }
 }
 
-function toggleService(player) {
+function updateService(operation) {
+    let player1_score = document.getElementById("player1-score");
+    let player2_score = document.getElementById("player2-score");
+
     let player1_action = document.getElementById("player1-action");
     let player2_action = document.getElementById("player2-action");
+
+    let total_score = parseInt(player1_score.innerText) + parseInt(player2_score.innerText);
+
+    if (total_score == 0) {
+        let rand = Math.floor(Math.random() * 2);
+        if (rand == 0) {
+            player1_action.classList.add("active");
+        } else {
+            player2_action.classList.add("active");
+        }
+    }
 
     let isActive1 = player1_action.classList.contains("active");
     let isActive2 = player2_action.classList.contains("active");
 
-    if (player === "player1" && !isActive1 && !isActive2) {
-        player1_action.classList.add("active");
-    } else if (player === "player1" && isActive1 && !isActive2) {
-        player1_action.classList.remove("active");
-    } else if (player === "player1" && !isActive1 && isActive2) {
-        player1_action.classList.add("active");
-        player2_action.classList.remove("active");
-    } else if (player === "player1" && isActive1 && isActive2) {
-        player1_action.classList.remove("active");
-    } else if (player === "player2" && !isActive1 && !isActive2) {
-        player2_action.classList.add("active");
-    } else if (player === "player2" && isActive1 && !isActive2) {
-        player1_action.classList.remove("active");
-        player2_action.classList.add("active");
-    } else if (player === "player2" && !isActive1 && isActive2) {
-        player2_action.classList.remove("active");
-    } else if (player === "player2" && isActive1 && isActive2) {
-        player2_action.classList.remove("active");
+    if ((total_score % 2 == 1) && (operation === "decrement")) {
+        if (isActive1) {
+            player1_action.classList.remove("active");
+            player2_action.classList.add("active");
+        } else if (isActive2) {
+            player2_action.classList.remove("active");
+            player1_action.classList.add("active");
+        }
+    }
+
+    if ((total_score % 2 == 0) && (operation !== "decrement")) {
+        if (isActive1) {
+            player1_action.classList.remove("active");
+            player2_action.classList.add("active");
+        } else if (isActive2) {
+            player2_action.classList.remove("active");
+            player1_action.classList.add("active");
+        }
     }
 }
-
-// function updateService(score) {
-
-// }
